d5d10bec5f9144c4cd60d5c2f907ace5e8ddbbf3
plokis <167650528+plokis@users.noreply.github.com>
2025-03-09 19:41:41 +0300

    Update CheckoutTest.java

--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -6,7 +6,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CheckoutTest extends BaseTest{
-
+    
+    /*
+    1. Открыть страницу магазина
+    2. Нажать кнопку товара Samsung galaxy s6
+    3. Нажать кнопку add to cart на странице товара
+    4. Нажать кнопку Ok в Alert окне
+    5. Нажать кнопку Cart
+    6. Ввести кнопку Place Order
+    7. Ввести данные покупателя и нажать кнопку Continue
+    8. Проверить есть ли иконка с зеленой галочкой
+     */
+    
     @Test
     public void checkout() {
         homePage.open();
@@ -15,7 +26,7 @@ public class CheckoutTest extends BaseTest{
         wait.until(ExpectedConditions.alertIsPresent()).accept();
         productPage.clickShoppingCart();
         cartPage.clickPlaceOrderButton();
-        cartPage.fillInfoPurchaseWindow("c","c","c","c","c","c");
+        cartPage.fillInfoPurchaseWindow("Ivan Ivanov","Russia","Moscow","456","09","26");
         Assert.assertTrue(cartPage.checkSuccessfulOrderIcon());
     }
 }
